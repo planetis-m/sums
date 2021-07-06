0939cbf134530bed0cad98af8e78d00d70179619
Antonis Geralis <43617260+planetis-m@users.noreply.github.com>
2021-07-06 16:10:23 +0300

    Create nakefile.nim

---
+++ nakefile.nim
@@ -0,0 +1,24 @@
+import nake, std/strformat
+
+task "docs", "Generate documentation":
+  # https://nim-lang.github.io/Nim/docgen.html
+  let
+    sums = "sums"
+    src = sums.addFileExt(".nim")
+    dir = "docs/"
+    doc = dir / sums.addFileExt(".html")
+    url = "https://github.com/planetis-m/sums"
+  if doc.needsRefresh(src):
+    echo "Generating the docs..."
+    direShell(nimExe,
+        &"doc --project --verbosity:0 --git.url:{url} --git.devel:master --git.commit:master --out:{dir} {src[0]}")
+    withDir(dir):
+      moveFile("theindex.html", "index.html")
+  else:
+    echo "Skipped generating the docs."
+
+task "test", "Run the tests":
+  withDir("tests/"):
+    for f in walkFiles("t*.nim"):
+      echo "Running test ", f, "..."
+      direShell(nimExe, &"c -r --hints:off -w:off --path:../ {f}")
